f565470d62cd1918d0863a325ef183aa84458239
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-31 20:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//your JS code here. If required.
+ document.getElementById("enterBtn").addEventListener("click", function() {
+            document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
+        });
